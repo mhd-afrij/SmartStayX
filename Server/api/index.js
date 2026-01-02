07050d474f88351b5f80d28bf1fe4abf4538c53c
mhd-afrij <mbmafrij@gmail.com>
2026-01-02 15:22:39 +0530

    Update Vercel configuration: change destination for rewrites from server.js to /api for improved routing.

---
+++ Server/api/index.js
@@ -0,0 +1,7 @@
+// Vercel serverless function handler for Express app
+import app from '../server.js';
+
+// Export the Express app as the handler
+// Vercel will automatically wrap this for serverless execution
+export default app;
+
